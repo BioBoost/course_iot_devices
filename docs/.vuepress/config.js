843c5eb2db019cbfce1868a058d0403e2b7d554b
Nico De Witte <nico.dewitte@outlook.com>
2019-09-19 15:59:07 +0200

    Start of new course on IoT devices

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  title: 'IoT Devices',
+  description: 'IoT Devices Course for VIVES University of Applied Sciences (Bachelor Degree)',
+  themeConfig: {
+    nav: [
+      {text: 'GitHub', link: 'Github.com'},
+    ],
+    sidebar: [
+      '/',
+      '/01_crash_course_nodejs/',
+    ],
+    sidebarDepth: 1,
+    repo: 'BioBoost/course_programming_from_base_to_ace',
+    docsDir: 'docs',
+    docsBranch: 'master'
+  },
+  markdown: {
+    lineNumbers: true,
+  },
+  serviceWorker: true,
+  plugins: [
+    ['vuepress-plugin-zooming', {
+      // selector for images that you want to be zoomable
+      // default: '.content img'
+      selector: 'img',
+
+      // make images zoomable with delay after entering a page
+      // default: 500
+      // delay: 1000,
+
+      // options of zooming
+      // default: {}
+      options: {
+        bgColor: 'black',
+        zIndex: 10000,
+      },
+    }],
+    ['container', {
+      type: 'codeoutput',
+      defaultTitle: 'Output',
+    }]
+  ],
+}
---
+++ docs/01_crash_course_nodejs/README.md
@@ -0,0 +1,7 @@
+---
+description: This chapter will introduce the most important concepts of NodeJS.
+title: 01 - Crash Course NodeJS
+---
+
+# Chapter 01 - Crash Course NodeJS
+
