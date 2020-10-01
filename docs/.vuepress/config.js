d2cac1e773c03147b1f0218efdb6dd80c485d60c
Nico De Witte <nico.dewitte@outlook.com>
2020-10-01 08:40:35 +0200

    Disable the old chapters

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,8 +7,8 @@ module.exports = {
     ],
     sidebar: [
       '/',
-      '/01_crash_course_nodejs/',
-      '/02_bluetooth_on_rpi/',
+      // '/01_crash_course_nodejs/',
+      // '/02_bluetooth_on_rpi/',
     ],
     sidebarDepth: 1,
     repo: 'BioBoost/course_programming_from_base_to_ace',
